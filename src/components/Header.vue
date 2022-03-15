f4e0ea804bdcfef368c5a9e5509d28edc0abd8d4
savannah-borst <savannah.borst@gmail.com>
2022-03-15 16:36:02 +0100

    change header to own component

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <header class="container-fluid header">
+        <h1 class="text-center text-white">Trivia Game</h1>
+    </header>
+</template>
+
+<style scoped>
+.header {
+        background-color: #0C304A;
+    }
+</style>
