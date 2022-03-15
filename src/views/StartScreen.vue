5017b27f64c88d2c8bad829cf775a7669acd2eab
savannah-borst <savannah.borst@gmail.com>
2022-03-15 15:02:54 +0100

    adding enter username/number of Q / difficulty

--- src/views/StartScreen.vue
+++ src/views/StartScreen.vue
@@ -9,7 +9,29 @@
     </header>
 
     <section>
-        <h2>Enter Username</h2>
+        <div>
+            <label for="username">Enter Username:</label>
+            <input type="text" id="username" name="username">
+        </div>
+
+        <div>
+            <label  for="amountQ">Number of Questions:</label>
+            <input type="number" id="amountQ" name="amountQ" min="1">
+        </div>
+
+        <div>
+            <h4>Difficulty</h4>
+
+            <input type="radio" id="difficulty" name="difficulty" value="easy">
+            <label for="easy">Easy</label>
+
+            <input type="radio" id="difficulty" name="difficulty" value="medium">
+            <label for="medium">Medium</label>
+
+            <input type="radio" id="difficulty" name="difficulty" value="hard">
+            <label for="hard">Hard</label>
+        </div>
+        
     </section>
 </template>
 
