8c4a779cdb4f70f0311fb7363fa06b3204cb97e0
savannah-borst <savannah.borst@gmail.com>
2022-03-16 18:07:27 +0100

    added styling and url checks and changes

--- src/api/questions.js
+++ src/api/questions.js
@@ -1,12 +1,12 @@
 import { BASE_URL_QUESTIONS } from '.';
 
 // GET: get array of questions
-export function getQuestions(nrOfQuestions, category, difficulty) {
+export function getQuestions(url) {
     // nrOfQuestions: number
     // category: number
     // difficulty: {Easy, Medium, Hard}
     // type: {multiple, boolean}
-    fetch(`${BASE_URL_QUESTIONS}amount=${nrOfQuestions}&category=${category}&difficulty=${difficulty}&type=multiple`)
+    fetch(`${BASE_URL_QUESTIONS}${url}`)
     .then(response => response.json())
     .then((results) => {
         // results will be an array of questions
