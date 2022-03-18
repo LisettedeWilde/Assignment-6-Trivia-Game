eb8aa1f4f28ca90145fb7bfbfcb82a1274992f7a
savannah-borst <savannah.borst@gmail.com>
2022-03-18 15:20:35 +0100

    rework: question test update user

--- src/components/QuestionForm.vue
+++ src/components/QuestionForm.vue
@@ -1,5 +1,4 @@
 <script setup>
-import AnswerButton from "../components/AnswerButton.vue";
 import decodeHtml from "../utils/htmlDecoder.js"
 
 const props = defineProps({
@@ -7,15 +6,21 @@ const props = defineProps({
   nrOfQuestions: Number,
   questionNr: Number,
   answers: Array,
+  answerText: {
+    type: String,
+    required: true,
+  },
 });
 
+// event when button gets clicked
+const emit = defineEmits(["onAnswerSelected"]);
 
-
-const emit = defineEmits(["onAnswerSelected"])
-
-const handleAnswerSelect = (e) => {
+// calls event
+const onButtonClick = (e) => {
   emit("onAnswerSelected", e);
-}
+  
+};
+
 </script>
 
 <template>
@@ -26,16 +31,14 @@ const handleAnswerSelect = (e) => {
       <h4>Question {{ props.questionNr }} of {{props.nrOfQuestions}}</h4>
     </div>
     <div class="d-flex justify-content-center">
-      <h3>{{ props.question }}</h3>
+      <h3>{{ decodeHtml(props.question) }}</h3>
     </div>
     <div class="btn-group-vertical text-white answer-buttons-form">
-      <button
+      <div
         v-for="a in props.answers"
-        :key="a"
-        class="btn-group-vertical answer-buttons mt-4"
-      >
-        <AnswerButton @onAnswerSelect="handleAnswerSelect($event)" :answerText="decodeHtml(a)" />
-      </button>
+        :key="a"> 
+        <button class="btn mt-4 btn-primary rounded border-0" @click="onButtonClick(a)"> {{ decodeHtml(a) }} </button>
+      </div>
     </div>
   </section>
 </template>
@@ -45,12 +48,12 @@ const handleAnswerSelect = (e) => {
   background-color: #0c304a;
 }
 
-.answer-buttons {
+.btn {
   width: 549px;
   background-color: #2DC0CC;
 }
 
-.answer-buttons:hover {
+.btn:hover {
   background-color: #EF3C67;
 }
 
